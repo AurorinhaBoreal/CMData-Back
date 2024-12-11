870d574c3cda674d5967498f84c6f6886ca776dc
Aurora Kruschewsky <AurorinhaBoreal@users.noreply.github.com>
2024-12-10 22:27:58 -0300

    feat: Implement user List, Register and Login

---
+++ src/controller/userController.js
@@ -0,0 +1,85 @@
+const { StatusCodes, ReasonPhrases} = require('http-status-codes')
+const User = require('../model/user')
+const { Router } = require('express');
+const { Sequelize } = require('sequelize');
+
+const route = Router();
+
+route.get('/list', async (req, res) => {
+    User.findAll()
+        .then((response) => {
+            return res.status(StatusCodes.OK).send({
+                errorStatus: false,
+                messageStatus: ReasonPhrases.OK,
+                data: response
+            })
+        })
+        .catch((error) => {
+            return res.status(StatusCodes.BAD_REQUEST).send({
+                errorStatus: true,
+                messageStatus: "Error listing users!",
+                errorObject:error
+            })
+        })
+})
+
+route.post('/register', async (req, res) => {
+
+    const { email, password, firstName, lastName, nickname} = req.body;
+
+    User.create({
+        email,
+        password,
+        firstName,
+        lastName,
+        nickname
+    })
+        .then(()=>{
+            return res.status(StatusCodes.CREATED).send({
+                errorStatus: false,
+                messageStatus: ReasonPhrases.CREATED,
+                message: "You have been successful registered! Redirecting..."
+            })
+        })
+        .catch((error) => {
+            return res.status(StatusCodes.BAD_REQUEST).send({
+                errorStatus: true,
+                messageStatus: ReasonPhrases.BAD_REQUEST,
+                errorObject:error
+            })
+        })
+})
+
+route.post('/login', async (req, res) => {
+
+    const { email, password} = req.body;
+
+    try {
+        const userFound = await User.findOne({
+            where: {
+                email: email
+            }
+        })
+
+        if (userFound.dataValues.password === password) {
+            return res.status(StatusCodes.ACCEPTED).send({
+                errorStatus: false,
+                messageStatus: ReasonPhrases.ACCEPTED,
+                message: "Your login is successfull! Redirecting..."
+            })
+        } else {
+            return res.status(StatusCodes.BAD_REQUEST).send({
+                errorStatus: false,
+                messageStatus: ReasonPhrases.BAD_REQUEST,
+                message: "The combination of email and password doesn't match!"
+            })
+        }
+    } catch (error) {
+        return res.status(StatusCodes.BAD_REQUEST).send({
+            errorStatus: true,
+            messageStatus: ReasonPhrases.BAD_REQUEST,
+            errorObject: error
+    })}
+})
+
+module.exports = route;
